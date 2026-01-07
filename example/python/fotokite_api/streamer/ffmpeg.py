90be19198e7e8e4eede0e4f809608a58c383df51
Marcel Schneider <marcel@cascode.eu>
2026-01-07 10:28:44 +0100

    Use `start_new_session

--- example/python/fotokite_api/streamer/ffmpeg.py
+++ example/python/fotokite_api/streamer/ffmpeg.py
@@ -65,7 +65,7 @@ def start_ffmpeg(rtsp_url: str, hls_dir: str) -> subprocess.Popen[bytes]:
     ]
 
     process = subprocess.Popen(
-        cmd, stdout=sys.stdout, stderr=sys.stderr, preexec_fn=os.setsid
+        cmd, stdout=sys.stdout, stderr=sys.stderr, start_new_session=True,
     )
     logging.info(f"Started FFmpeg for {rtsp_url}")
     return process
